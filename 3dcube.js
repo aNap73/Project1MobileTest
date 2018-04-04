aa907f4bca4bb54acc9188011c8d075bf5cdd204
aNap73 <napolitano.anthony@yahoo.com>
2018-04-04 16:39:07 -0400

    o noes

--- 3dcube.js
+++ 3dcube.js
@@ -1,5 +1,5 @@
 //A.Napolitano  03/22/2018
-//v.0.0.0.1.5 - 04/04/2018
+//v.0.0.0.1.6 - 04/04/2018
 //ant3d is a simple api extraction and 3d interface written in THREE.js
 //It currently displays data from the API's: Wikipedia, Giffy and You Tube
 //The script is interfaced by calling the ant3d.Startup method
